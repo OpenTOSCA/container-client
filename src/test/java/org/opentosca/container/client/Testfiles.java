8d4430526c175543454ca6a578d6e33a90b4013d
Domas <domasm@live.de>
2019-12-11 12:47:57 +0100

    fixed the test execution, added support for multiple yaml files

---
+++ src/test/java/org/opentosca/container/client/Testfiles.java
@@ -0,0 +1,21 @@
+package org.opentosca.container.client;
+
+import java.util.List;
+
+
+public class Testfiles {
+
+    
+        private List<CSARTest> tests;
+
+        public List<CSARTest> getTests(){
+            return tests;
+        }
+        public void setTests(List<CSARTest> tests){
+            this.tests=tests;
+        }
+    
+
+       
+        
+}
